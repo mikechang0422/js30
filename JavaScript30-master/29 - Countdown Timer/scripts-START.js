2fb6160384d65d2357cb2f7a8944be3b012155d5
mike <mikechang0422@gmail.com>
2021-10-03 18:37:23 +0800

    增加 requestAnimationFrame 寫法 其用途可以用在像是canvas 等吃畫面更新的程式上

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -7,27 +7,47 @@
         const now = Date.now()// = +new Date() = Date.now() = new Date().valueof()  +可以轉型但不要用會被貶XD 2,3,4或new Date.getTime()都可以
         const end = now + sec * 1000//因為now是毫秒
         //倒數計時
-        setCountDown(end)
+        //setCountDown(end)
+        setAnimationFrame(end)
         //顯示最後時間
         showEndTime(end)
     }
 
     //倒數計時的function
-    function setCountDown(end){
-        clearInterval(timer)//啟動前先清除舊的計時器
-        timer = setInterval(function(){
-            const secLeft = Math.floor((end - Date.now()) / 1000)//結束的時間 - 當前時間
+    // function setCountDown(end){
+    //     clearInterval(timer)//啟動前先清除舊的計時器
+    //     timer = setInterval(function(){
+    //         const secLeft = Math.floor((end - Date.now()) / 1000)//結束的時間 - 當前時間
+    //         if(secLeft >= 0 ){
+    //             let displayMin = Math.floor(secLeft / 60)//floor無條件捨去
+    //             const displaySec = secLeft % 60
+    //             displayMin = displayMin < 10 ? '0' + displayMin : displayMin//這判斷是主要是拿來補畫面ㄉ0
+    //             timeLeft.innerText = `${displayMin}:${displaySec}`
+    //         } else {
+    //             timeLeft.innerHTML = 'Happy Finish !'
+    //             clearInterval(timer)
+    //         }
+    //     },16)//刷新時間 1sec / 16 正常設16因為60ps=1秒16次
+    // }
+
+    //改用setAnimationFrame做
+    function setAnimationFrame(end){
+        window.cancelAnimationFrame(timer)
+        timer = window.requestAnimationFrame(function(){
+            const secLeft = Math.floor((end - Date.now()) / 1000)
             if(secLeft >= 0 ){
-                let displayMin = Math.floor(secLeft / 60)//floor無條件捨去
+                let displayMin = Math.floor(secLeft / 60)
                 const displaySec = secLeft % 60
-                displayMin = displayMin < 10 ? '0' + displayMin : displayMin//這判斷是主要是拿來補畫面ㄉ0
+                displayMin = displayMin < 10 ? '0' + displayMin : displayMin
                 timeLeft.innerText = `${displayMin}:${displaySec}`
+                setAnimationFrame(end)
             } else {
                 timeLeft.innerHTML = 'Happy Finish !'
-                clearInterval(timer)
+                
             }
-        },16)//刷新時間 1sec / 16 正常設16因為60ps=1秒16次
+        })
     }
+
     //顯示最後時間的function
     function showEndTime(time){//也可以不用這個function但拆開可以讓每個function變小而且有針對性
         const endDate = new Date(time)
@@ -58,28 +78,39 @@
 
 
     //額外補充
-    const switchMap = {
-        1: A,
-        2: B,
-        3: C
-    }
-    function text(aa){
-        if(switchMap[aa]) switchMap[aa]()
-        else D()
+    // const switchMap = {
+    //     1: A,
+    //     2: B,
+    //     3: C
+    // }
+    // function text(aa){
+    //     if(switchMap[aa]) switchMap[aa]()
+    //     else D()
 
-        //上方寫法+宣告等於下面的switch但從判斷是變查找 更容易理解
-        // switch(aa){
-        //     case 1;
-        //     A()
-        //     break
-        //     case 2
-        //     B()
-        //     break
-        //     case 3
-        //     C()
-        //     break
-        //     default
-        //     D()
-        // }
-    }
+    //上方寫法+宣告等於下面的switch但從判斷是變查找 更容易理解
+    // switch(aa){
+    //     case 1;
+    //     A()
+    //     break
+    //     case 2
+    //     B()
+    //     break
+    //     case 3
+    //     C()
+    //     break
+    //     default
+    //     D()
+    // }
+    // }
+
+    //教學 requestAnimationFrame 畫面更新頻率
+//     let limit = 100
+//     const rafCallback = function(){
+//         console.log(limit)
+//         if(limit > 0){
+//             limit--
+//             requestAnimationFrame(rafCallback)//要再呼叫自己一次 讓他循環執行
+//         }
+//     }
+//     window.requestAnimationFrame(rafCallback)//啟動
 })()
